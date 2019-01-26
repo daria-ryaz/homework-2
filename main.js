d1eddd118cdf04c57866d6a5fc40cc0a7d4a30c7
Daria Ryazanova <ya.dryazanova@gmail.com>
2019-01-26 17:20:32 +0300

    change in main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 //если значение попадает в диапазон 18-64 - "Работа"
 //если значение попадает в диапазон 65-100 - "Пенсия"
 
-var age = 17;
+// var age = 17;
 // if (1 <= age && age <= 17) {
 //     alert ('Учеба');
 // }
@@ -14,32 +14,24 @@ var age = 17;
 //     alert('Пенсия');
 // };
 
-var messege = (1 <= age && age <= 17) ? 'Учеба' : (18 <= age && age <= 64) ? 'Работа' : 'Пенсия';
-
-console.log(messege);
+// var messege = (1 <= age && age <= 17) ? 'Учеба' : (18 <= age && age <= 64) ? 'Работа' : 'Пенсия';
+//
+// console.log(messege);
 
 // Создайте массив с числами от 1 до 9. С помощью цикла for и условной инструкции switch выведите в консоль: 1 разработчик,2,3...
 
-// var arr = [1,2,3,4,5,6,7,8,9];
-//
-// for (var a in arr) {
-//     switch (arr[a]) {
-//         case 1: console.log(arr[a] + ' разработчик');
-//         break;
-//         case 2: console.log(arr[a] + ' разработчика');
-//         break;
-//         case 3: console.log(arr[a] + ' разработчика');
-//         break;
-//         case 4: console.log(arr[a] + ' разработчика');
-//         break;
-//         case 5: console.log(arr[a] + ' разработчиков');
-//         break;
-//         case 6: console.log(arr[a] + ' разработчиков');
-//         break;
-//         case 7: console.log(arr[a] + ' разработчиков');
-//         break;
-//         case 8: console.log(arr[a] + ' разработчиков');
-//         break;
-//         case 9: console.log(arr[a] + ' разработчиков');
-//     }
-// }
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (var i = 0; i < arr.length; i++) {
+    var endStr = '';
+
+    switch (arr[i]) {
+        case 1: break;
+        case 2:
+        case 3:
+        case 4: endStr = 'а'; break;
+        default: endStr = 'ов';
+    }
+
+    console.log(arr[i] + ' разработчик' + endStr)
+}
